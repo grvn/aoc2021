f8af84cfafa7c7e47da6a9cf0b874cad7528d941
riched <rickard.hedlund@lm.se>
2021-12-14 15:55:50 +0100

    part 1

--- day10/part1.go
+++ day10/part1.go
@@ -7,6 +7,21 @@ import (
 	"github.com/grvn/aoc2021/util"
 )
 
+var (
+	closes = map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+		'<': '>',
+	}
+	points = map[rune]int{
+		')': 3,
+		']': 57,
+		'}': 1197,
+		'>': 25137,
+	}
+)
+
 func Part1() *cobra.Command {
 	return &cobra.Command{
 		Use:   "1",
@@ -20,5 +35,27 @@ func Part1() *cobra.Command {
 }
 
 func execute1(input *util.Input) int {
-	return 0
+	errorscore := 0
+	for line := range input.GetLines() {
+		queue := util.NewQueue()
+		for _, char := range line {
+			if close, ok := closes[char]; ok {
+				queue.Push(close)
+				continue
+			}
+			top, ok := queue.Peek()
+			if !ok {
+				errorscore += points[char]
+				continue
+			}
+
+			if char == top.(rune) {
+				queue.Pop()
+				continue
+			}
+			errorscore += points[char]
+			break
+		}
+	}
+	return errorscore
 }
--- util/queues.go
+++ util/queues.go
@@ -20,10 +20,22 @@ func (queue *Queue) IsEmpty() bool {
 	return queue.values.Len() == 0
 }
 
+// Returns value and true or
+// nil and false indicating no value
+func (queue *Queue) Peek() (interface{}, bool) {
+	if queue.IsEmpty() {
+		return nil, false
+	} else {
+		return queue.values.Back().Value, true
+	}
+}
+
 func (queue *Queue) Push(item interface{}) {
 	queue.values.PushBack(item)
 }
 
+// Returns value and true or
+// nil and false indicating no value
 func (queue *Queue) Pop() (interface{}, bool) {
 	if queue.IsEmpty() {
 		return nil, false
